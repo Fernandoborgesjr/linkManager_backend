493959c666fd262e2d10cfde6e9a61ad51330993
Fernando Borges <fernandoborgesjr@outlook.com>
2020-07-05 16:08:00 -0300

    Storing accounts into db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcrypt');
+const { Account } = require('../src/models');
 
 router.get('/sign-in', (req, res) => {
     return res.json('Sign in');
 });
 
 
-router.get('/sign-up', (req, res) => {
-    return res.json('Sign up');
+router.get('/sign-up', async (req, res) => {
+    const email = 'fernandoborgesj8r@outlook.com';
+    const password = '123456';
+    const result = await Account.create({ email, password: bcrypt.hashSync(password, 10) });
+    console.log(result);
+    return res.json(result);
 });
 
 
---
+++ src/models/account.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Account = sequelize.define('Account', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    Account.prototype.toJSON = function () {
+        const values = { ...this.get() };
+        delete values.password;
+        return values;
+    };
+    return Account;
+};
